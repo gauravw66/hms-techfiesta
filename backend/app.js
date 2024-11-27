05a3e8f9f52b8cdc2920a1b0b81203a122d36c00
gaurav662004 <gbwaghmare66@gmail.com>
2024-11-27 23:11:27 +0530

    backend

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectToDatabase = require("./connection");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Connect to MongoDB
+connectToDatabase().then(() => {
+  console.log("Database connection established");
+});
+
+// Define a simple route
+app.get("/", (req, res) => {
+  res.send("welcome to jgmsa!");
+});
+
+// Start the server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
